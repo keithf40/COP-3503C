e7479143298b8da315a56d2d02fe093901faaf36
keithf40 <147755486+keithf40@users.noreply.github.com>
2023-10-14 00:16:33 -0400

    Update main.cpp

--- Lab 4/main.cpp
+++ Lab 4/main.cpp
@@ -6,6 +6,94 @@
 #include <fstream>
 using namespace std;
 
+class LegoSet
+{
+public:
+	int _number = 0;
+	string _theme;
+	string _name;
+	int _minifigs = 0;
+	int _pieces = 0;
+	double _price = 0;
+};
+
+vector<LegoSet> lego1;
+vector<LegoSet> lego2;
+vector<LegoSet> lego3;
+
+void ReadCSV(ifstream& file, vector<LegoSet>& legosets)
+{
+	while (file.eof() == false)
+	{
+		string buffer;
+
+		LegoSet legoset;
+
+		getline(file, buffer, ',');
+		legoset._number = stoi(buffer);
+
+		getline(file, buffer, ',');
+		legoset._theme = buffer;
+
+		getline(file, buffer, ',');
+		legoset._name = buffer;
+
+		getline(file, buffer, ',');
+		legoset._minifigs = stoi(buffer);
+
+		getline(file, buffer, ',');
+		legoset._pieces = stoi(buffer);
+
+		getline(file, buffer);
+		legoset._price = stod(buffer);
+
+		legosets.push_back(legoset);
+	}
+}
+
+int ReadCSV(const char* filename)
+{
+	ifstream file(filename);
+
+	string buffer;
+	getline(file, buffer);
+	if (file.is_open() == false)
+	{
+		cerr << "File is not open";
+		return -1;
+	}
+
+	ReadCSV(file, lego1);
+
+	return 0;
+}
+
+LegoSet FindMostExpensive(const vector<LegoSet>& legosets)
+{
+	LegoSet maxPrice;
+	maxPrice._price = 0;
+	for (const LegoSet& legoset : legosets)
+	{
+		if (legoset._price > maxPrice._price)
+			maxPrice = legoset;
+	}
+
+	return maxPrice;
+}
+
+LegoSet FindLargestPiece(const vector<LegoSet>& legosets)
+{
+	LegoSet maxPieces;
+	maxPieces._pieces = 0;
+	for (const LegoSet& legoset : legosets)
+	{
+		if (legoset._pieces > maxPieces._pieces)
+			maxPieces = legoset;
+	}
+
+	return maxPieces;
+}
+
 int main()
 {
 	cout << std::fixed << setprecision(2);
@@ -20,23 +108,14 @@ int main()
 	/*======= Load data from file(s) =======*/
 
 	if (option == 1)
-	{
-
-	}
-
+		ReadCSV("lego1.csv");
 	else if (option == 2)
-	{
-
-	}
-
+		ReadCSV("lego2.csv");
 	else if (option == 3)
-	{
-
-	}
-
+		ReadCSV("lego3.csv");
 	else
 	{
-
+		cout << "No data";
 	}
 
 	cout << "1. Most Expensive set" << endl;
@@ -50,24 +129,50 @@ int main()
 	cout << "0. Exit" << endl;
 
 	int choice;
-	cin >> choice;
-	cin.get();  // Clear newline character for any later input
+	std::cin >> choice;
+	std::cin.get();  // Clear newline character for any later input
 
 	/*======= Print out how many sets were loaded =======*/
+
 	/*======= Based on the choice, execute the appropriate task and show the results =======*/
 
 	if (choice == 1)
 	{
+		LegoSet max = FindMostExpensive(lego1);
 
+		cout << "Total number of sets: " << lego1.size() << endl;
+		cout << "The most expensive set is:" << endl;
+		cout << "Name: " << max._name << endl;
+		cout << "Number: " << max._number << endl;
+		cout << "Theme: " << max._theme << endl;
+		cout << "Price: $" << max._price << endl;
+		cout << "Minifigures: " << max._minifigs << endl;
+		cout << "Piece count: " << max._pieces << endl;
 	}
 
 	else if (choice == 2)
 	{
+		LegoSet max = FindLargestPiece(lego1);
 
+		std::cout << "Total number of sets: " << lego1.size() << endl;
+		std::cout << "The set with the highest parts count:" << endl;
+		std::cout << "Name: " << max._name << endl;
+		std::cout << "Number: " << max._number << endl;
+		std::cout << "Theme: " << max._theme << endl;
+		std::cout << "Price: $" << max._price << endl;
+		std::cout << "Minifigures: " << max._minifigs << endl;
+		std::cout << "Piece count: " << max._pieces << endl;
 	}
 
 	else if (choice == 3)
 	{
+		string input;
+		cin >> input;
+
+
+		
+
+			
 
 	}
 
@@ -90,8 +195,8 @@ int main()
 	{
 
 	}
-	
-	else 
+
+	else if (choice == 8)
 	{
 
 	}

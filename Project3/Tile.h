086fa39ba69069af17bb44407e894c297c38d4b6
keithf40 <147755486+keithf40@users.noreply.github.com>
2023-11-29 02:04:21 -0500

    Update Tile.h

--- Project3/Tile.h
+++ Project3/Tile.h
@@ -9,12 +9,20 @@ using std::vector;
 class Tile
 {
 public:
+	sf::Sprite _sprite;
 	sf::Sprite _tileSpriteHidden;
 	sf::Sprite _tileSpriteRevealed;
 	sf::Sprite _tileSpriteMine;
 	sf::Sprite _tileSpriteFlag;
 	sf::Sprite _tileBlown;
-	sf::Sprite _sprite;
+	sf::Sprite _tileSprite1;
+	sf::Sprite _tileSprite2;
+	sf::Sprite _tileSprite3;
+	sf::Sprite _tileSprite4;
+	sf::Sprite _tileSprite5;
+	sf::Sprite _tileSprite6;
+	sf::Sprite _tileSprite7;
+	sf::Sprite _tileSprite8;
 	bool _isRevealed;
 	bool _isMine;
 	bool _isFlag;
@@ -22,6 +30,7 @@ public:
 	unsigned int _column;
 	vector<Tile*> _adjacentTiles;
 	int _flagCounter = 0;
+	int _neighbors = 0;
 public:
 	Tile(unsigned int row, unsigned int column);
 	sf::Sprite& GetSprite();
